098b4887b34e9b1676a03500244f41f6edf80441
HJ <113006963+himediahj@users.noreply.github.com>
2023-08-16 13:22:17 +0900

    Update OrderContext.js

--- client/src/context/OrderContext.js
+++ client/src/context/OrderContext.js
@@ -43,16 +43,11 @@ export function OrderContextProvider(props) {
 
     const value = useMemo(() => {
         function updateItemCount(itemName, newItemCount, optionType) {
-            // get option Map and make a copy
-            const oldOrderMap = orderCounts[optionType];
-            const newOrderMap = new Map(oldOrderMap);
-            // update the copied Map
-            newOrderMap.set(itemName, parseInt(newItemCount));
-
-            // create new object with the old orderCounts plus new map
             const newOrderCounts = { ...orderCounts };
-            newOrderCounts[optionType] = newOrderMap;
 
+            const orderCountMap = orderCounts[optionType];
+            orderCountMap.set(itemName, parseInt(newItemCount));
+            
             // update state
             setOrderCounts(newOrderCounts);
         }
